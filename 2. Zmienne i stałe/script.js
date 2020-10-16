f4a7e4d1b6dc6a6d8729e519bc7bc903bdee9c89
msznober <msznober@op.pl>
2020-10-16 22:14:23 +0200

    Zadania 1-3

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,18 @@
+/*Zadanie 1*/
+let number = 10;
+let text = "abc";
+let numText = 2 + "dwa";
+let logic = true;
+let special = undefined;
+console.log(number,text,numText,logic,special);
+
+/*Zadanie 2*/
+let num1 = 5;
+let num2 = 22;
+let sum = 0;
+sum = num1+num2;
+console.log(sum);
+
+/*Zadanie 3*/
+let num;
+console.log(num); //zmienna niezdefiniowana
---
+++ 3.Operatory/script.js
@@ -0,0 +1,39 @@
+/*Zadanie 1*/
+let first = true;
+let second = false;
+console.log(true == false);
+
+/*Zadanie 2*/
+let num1 = 5;
+let num2 = 4;
+moduloResult = 0;
+moduloResult = num1%num2;
+console.log(moduloResult);
+
+/*Zadanie 3*/
+let str1 = "to jest";
+let str2 = "string";
+let stringsResult = 0;
+stringsResult = str1 + ' ' + str2;
+console.log(stringsResult);
+
+/*Zadanie 4*/
+var someNumber = 425;
+var someString = "425";
+console.log(someNumber == someString); //true poniewa¿ == nie porównuje typów danych
+console.log(someNumber === someString); //false poniewa¿ === porównuje typy danych
+
+/*Zadanie 5*/
+let counter = 30;
+counter++;
+console.log(counter);
+counter--;
+console.log(counter);
+
+/*Zadanie 6*/
+let number1 = 55;
+let number2 = 66;
+result = null;
+result = number1 > number2;
+console.log(result);
+
